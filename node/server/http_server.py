NapthaAI/node

- Added SecretInput model to schema - Added Secret class with generate key pairs, encrypt and decrypt functions - Added /public-key, /user/secret/create endpoints - Added env injection for modules - Added key_generation and secret_generation to server.py - Added functions to query api_secrets table - Refactored api_secrets.surql

node/server/http_server.py
@@ -3,10 +3,12 @@ import httpx
 import logging
 import traceback
 from datetime import datetime
-from typing import Union, Any, Dict
+from typing import Union, Any, Dict, List, Optional
 import uvicorn
-from dotenv import load_dotenv
-from fastapi import FastAPI, APIRouter, HTTPException, Request
+import os
+import base64
+from dotenv import load_dotenv, dotenv_values
+from fastapi import FastAPI, APIRouter, HTTPException, Request, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
@@ -38,20 +40,42 @@ from node.schemas import (
     KBRun,
     ModuleExecutionType,
     ToolDeployment,
+    SecretInput
 )
 from node.storage.db.db import LocalDBPostgres
 from node.storage.hub.hub import HubDBSurreal
 from node.user import check_user, register_user, get_user_public_key, verify_signature
 from node.config import LITELLM_URL
 from node.worker.docker_worker import execute_docker_agent
-from node.worker.template_worker import run_agent, run_tool, run_environment, run_orchestrator, run_kb, run_memory
+from node.worker.template_worker import run_agent, run_tool, run_environment, run_orchestrator, run_kb, run_memory, set_env_variables
 from node.client import Node as NodeClient
 from node.storage.server import router as storage_router
+from node.secret import Secret
+from contextlib import contextmanager
 
 logger = logging.getLogger(__name__)
 load_dotenv()
 
 LITELLM_HTTP_TIMEOUT = 60*5
+
+@contextmanager
+def set_env_variables():
+    dot_env_vars = dotenv_values(os.path.join(os.path.dirname(__file__), '../../.env'))
+    old_env = os.environ.copy()
+
+    try:
+        # Empty any environment variables that come from the .env file
+        for key in dot_env_vars:
+            if key in os.environ:
+                os.environ[key] = ""
+        yield
+    except Exception as e:
+        logger.error(e)
+    finally:
+        # Restore the original environment variables after exiting the context
+        os.environ.clear()
+        os.environ.update(old_env)
+
 
 class TransientDatabaseError(Exception):
     pass
@@ -66,6 +90,7 @@ class HTTPServer:
         router = APIRouter()
         self.server = None
         self.should_exit = False
+        self.secret = Secret()
 
         @self.app.on_event("shutdown")
         async def shutdown_event():
@@ -91,6 +116,15 @@ class HTTPServer:
                     "body": await request.json()
                 }
             )
+        
+        @router.get("/public_key")
+        async def get_public_key():
+            try:
+                public_key = self.secret.get_public_key()
+
+                return {"public_key" : public_key}
+            except Exception as e:
+                return {"error": f"Error retrieving public key: {str(e)}"}
 
         @router.post("/agent/create")
         async def agent_create_endpoint(agent_input: AgentDeployment) -> AgentDeployment:
@@ -270,6 +304,22 @@ class HTTPServer:
             if '_sa_instance_state' in response:
                 response.pop('_sa_instance_state')
             return response
+        
+        @router.post("/user/secret/create")
+        async def user_secret_create_endpoint(secrets: List[SecretInput], is_update: Optional[bool] = Query(False)):
+            try:
+                aes_secret = base64.b64decode(os.getenv("AES_SECRET"))
+                for data in secrets:
+                    rsa_decrypted_secret_value = self.secret.decrypt_rsa(data.secret_value)
+                    encrypted_secret_value = self.secret.encrypt_with_aes(rsa_decrypted_secret_value, aes_secret)
+                    data.secret_value = encrypted_secret_value
+                
+                async with HubDBSurreal() as hub:
+                    result = await hub.create_secret(secrets, is_update)
+                
+                return result
+            except Exception as e:
+                raise HTTPException(status_code=400, detail=f"Error processing secrets: {str(e)}")
 
         async def get_server_connection(self, server_id: str):
             """
@@ -419,43 +469,53 @@ class HTTPServer:
 
             if not verify_signature(module_run_input.consumer_id, module_run_input.signature, user_public_key):
                 raise HTTPException(status_code=401, detail="Unauthorized: Invalid signature")
+            
+            user_env_data = {}
+            async with HubDBSurreal() as hub:
+                secret = Secret()
+                secret_data = await hub.get_user_secrets(module_run_input.consumer_id)
+                
+                for record in secret_data:
+                    decrypted_value = secret.decrypt_with_aes(record["secret_value"], base64.b64decode(os.getenv("AES_SECRET")))
+                    user_env_data[record["key_name"]] = decrypted_value
 
-            # Create module run record in DB
-            async with LocalDBPostgres() as db:
-                module_run = await create_func(db)(module_run_input)
-                if not module_run:
-                    raise HTTPException(status_code=500, detail=f"Failed to create {module_type} run")
-                module_run_data = module_run.model_dump()
+            with set_env_variables():
+                # Create module run record in DB
+                async with LocalDBPostgres() as db:
+                    module_run = await create_func(db)(module_run_input)
+                    if not module_run:
+                        raise HTTPException(status_code=500, detail=f"Failed to create {module_type} run")
+                    module_run_data = module_run.model_dump()
 
-                logger.info(f"{module_type.capitalize()} run data: {module_run_data}")
+                    logger.info(f"{module_type.capitalize()} run data: {module_run_data}")
 
-            await self.register_user_on_worker_nodes(module_run)
+                await self.register_user_on_worker_nodes(module_run)
 
-            # Execute the task based on module type
-            if module_run_input.deployment.module.execution_type == ModuleExecutionType.package:
-                if module_type == "agent":
-                    _ = run_agent.delay(module_run_data)
-                elif module_type == "tool":
-                    _ = run_tool.delay(module_run_data)
-                elif module_type == "orchestrator":
-                    _ = run_orchestrator.delay(module_run_data)
-                elif module_type == "environment":
-                    _ = run_environment.delay(module_run_data)
-                elif module_type == "kb":
-                    _ = run_kb.delay(module_run_data)
-                elif module_type == "memory":
-                    _ = run_memory.delay(module_run_data)                   
-            elif module_run_input.deployment.module.execution_type == ModuleExecutionType.docker and module_type == "agent":
-                # validate docker params
-                try:
-                    _ = DockerParams(**module_run_data["inputs"])
-                except Exception as e:
-                    raise HTTPException(status_code=400, detail=f"Invalid docker params: {str(e)}")
-                _ = execute_docker_agent.delay(module_run_data)
-            else:
-                raise HTTPException(status_code=400, detail=f"Invalid {module_type} run type")
+                # Execute the task based on module type
+                if module_run_input.deployment.module.execution_type == ModuleExecutionType.package:
+                    if module_type == "agent":
+                        _ = run_agent.delay(module_run_data, user_env_data)
+                    elif module_type == "tool":
+                        _ = run_tool.delay(module_run_data, user_env_data)
+                    elif module_type == "orchestrator":
+                        _ = run_orchestrator.delay(module_run_data, user_env_data)
+                    elif module_type == "environment":
+                        _ = run_environment.delay(module_run_data, user_env_data)
+                    elif module_type == "kb":
+                        _ = run_kb.delay(module_run_data, user_env_data)
+                    elif module_type == "memory":
+                        _ = run_memory.delay(module_run_data, user_env_data)                   
+                elif module_run_input.deployment.module.execution_type == ModuleExecutionType.docker and module_type == "agent":
+                    # validate docker params
+                    try:
+                        _ = DockerParams(**module_run_data["inputs"])
+                    except Exception as e:
+                        raise HTTPException(status_code=400, detail=f"Invalid docker params: {str(e)}")
+                    _ = execute_docker_agent.delay(module_run_data)
+                else:
+                    raise HTTPException(status_code=400, detail=f"Invalid {module_type} run type")
 
-            return module_run_data
+                return module_run_data
         
         except HTTPException as e:
             logger.error(f"Error: {str(e.detail)}")

NapthaAI/naptha-node

node/server/http_server.py
@@ -3,10 +3,12 @@ import httpx
 import logging
 import traceback
 from datetime import datetime
-from typing import Union, Any, Dict
+from typing import Union, Any, Dict, List, Optional
 import uvicorn
-from dotenv import load_dotenv
-from fastapi import FastAPI, APIRouter, HTTPException, Request
+import os
+import base64
+from dotenv import load_dotenv, dotenv_values
+from fastapi import FastAPI, APIRouter, HTTPException, Request, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
@@ -37,16 +39,18 @@ from node.schemas import (
     KBRun,
     ModuleExecutionType,
     ToolDeployment,
+    SecretInput
 )
 from node.storage.db.db import LocalDBPostgres
 from node.storage.hub.hub import HubDBSurreal
 from node.user import check_user, register_user, get_user_public_key, verify_signature
 from node.worker.docker_worker import execute_docker_agent
-from node.worker.package_worker import run_agent, run_tool, run_environment, run_orchestrator, run_kb, run_memory
+from node.worker.package_worker import run_agent, run_tool, run_environment, run_orchestrator, run_kb, run_memory, set_env_variables
 from node.client import Node as NodeClient
 from node.storage.server import router as storage_router
 from node.inference.server import router as inference_router
-import os
+from node.secret import Secret
+from contextlib import contextmanager
 
 logger = logging.getLogger(__name__)
 load_dotenv()
@@ -61,6 +65,25 @@ LITELLM_URL = "http://litellm:4000" if os.getenv("LAUNCH_DOCKER") == "true" else
 if not LITELLM_MASTER_KEY:
     raise Exception("Missing LITELLM_MASTER_KEY for authentication")
 
+@contextmanager
+def set_env_variables():
+    dot_env_vars = dotenv_values(os.path.join(os.path.dirname(__file__), '../../.env'))
+    old_env = os.environ.copy()
+
+    try:
+        # Empty any environment variables that come from the .env file
+        for key in dot_env_vars:
+            if key in os.environ:
+                os.environ[key] = ""
+        yield
+    except Exception as e:
+        logger.error(e)
+    finally:
+        # Restore the original environment variables after exiting the context
+        os.environ.clear()
+        os.environ.update(old_env)
+
+
 class TransientDatabaseError(Exception):
     pass
 
@@ -73,6 +96,7 @@ class HTTPServer:
         router = APIRouter()
         self.server = None
         self.should_exit = False
+        self.secret = Secret()
 
         @self.app.on_event("shutdown")
         async def shutdown_event():
@@ -98,6 +122,15 @@ class HTTPServer:
                     "body": await request.json()
                 }
             )
+        
+        @router.get("/public_key")
+        async def get_public_key():
+            try:
+                public_key = self.secret.get_public_key()
+
+                return {"public_key" : public_key}
+            except Exception as e:
+                return {"error": f"Error retrieving public key: {str(e)}"}
 
         @router.post("/agent/create")
         async def agent_create_endpoint(agent_input: AgentDeployment) -> AgentDeployment:
@@ -277,6 +310,22 @@ class HTTPServer:
             if '_sa_instance_state' in response:
                 response.pop('_sa_instance_state')
             return response
+        
+        @router.post("/user/secret/create")
+        async def user_secret_create_endpoint(secrets: List[SecretInput], is_update: Optional[bool] = Query(False)):
+            try:
+                aes_secret = base64.b64decode(os.getenv("AES_SECRET"))
+                for data in secrets:
+                    rsa_decrypted_secret_value = self.secret.decrypt_rsa(data.secret_value)
+                    encrypted_secret_value = self.secret.encrypt_with_aes(rsa_decrypted_secret_value, aes_secret)
+                    data.secret_value = encrypted_secret_value
+                
+                async with HubDBSurreal() as hub:
+                    result = await hub.create_secret(secrets, is_update)
+                
+                return result
+            except Exception as e:
+                raise HTTPException(status_code=400, detail=f"Error processing secrets: {str(e)}")
 
         async def get_server_connection(self, server_id: str):
             """
@@ -404,43 +453,53 @@ class HTTPServer:
 
             if not verify_signature(module_run_input.consumer_id, module_run_input.signature, user_public_key):
                 raise HTTPException(status_code=401, detail="Unauthorized: Invalid signature")
+            
+            user_env_data = {}
+            async with HubDBSurreal() as hub:
+                secret = Secret()
+                secret_data = await hub.get_user_secrets(module_run_input.consumer_id)
+                
+                for record in secret_data:
+                    decrypted_value = secret.decrypt_with_aes(record["secret_value"], base64.b64decode(os.getenv("AES_SECRET")))
+                    user_env_data[record["key_name"]] = decrypted_value
 
-            # Create module run record in DB
-            async with LocalDBPostgres() as db:
-                module_run = await create_func(db)(module_run_input)
-                if not module_run:
-                    raise HTTPException(status_code=500, detail=f"Failed to create {module_type} run")
-                module_run_data = module_run.model_dump()
+            with set_env_variables():
+                # Create module run record in DB
+                async with LocalDBPostgres() as db:
+                    module_run = await create_func(db)(module_run_input)
+                    if not module_run:
+                        raise HTTPException(status_code=500, detail=f"Failed to create {module_type} run")
+                    module_run_data = module_run.model_dump()
 
-                logger.info(f"{module_type.capitalize()} run data: {module_run_data}")
+                    logger.info(f"{module_type.capitalize()} run data: {module_run_data}")
 
-            await self.register_user_on_worker_nodes(module_run)
+                await self.register_user_on_worker_nodes(module_run)
 
-            # Execute the task based on module type
-            if module_run_input.deployment.module.execution_type == ModuleExecutionType.package:
-                if module_type == "agent":
-                    _ = run_agent.delay(module_run_data)
-                elif module_type == "tool":
-                    _ = run_tool.delay(module_run_data)
-                elif module_type == "orchestrator":
-                    _ = run_orchestrator.delay(module_run_data)
-                elif module_type == "environment":
-                    _ = run_environment.delay(module_run_data)
-                elif module_type == "kb":
-                    _ = run_kb.delay(module_run_data)
-                elif module_type == "memory":
-                    _ = run_memory.delay(module_run_data)                   
-            elif module_run_input.deployment.module.execution_type == ModuleExecutionType.docker and module_type == "agent":
-                # validate docker params
-                try:
-                    _ = DockerParams(**module_run_data["inputs"])
-                except Exception as e:
-                    raise HTTPException(status_code=400, detail=f"Invalid docker params: {str(e)}")
-                _ = execute_docker_agent.delay(module_run_data)
-            else:
-                raise HTTPException(status_code=400, detail=f"Invalid {module_type} run type")
+                # Execute the task based on module type
+                if module_run_input.deployment.module.execution_type == ModuleExecutionType.package:
+                    if module_type == "agent":
+                        _ = run_agent.delay(module_run_data, user_env_data)
+                    elif module_type == "tool":
+                        _ = run_tool.delay(module_run_data, user_env_data)
+                    elif module_type == "orchestrator":
+                        _ = run_orchestrator.delay(module_run_data, user_env_data)
+                    elif module_type == "environment":
+                        _ = run_environment.delay(module_run_data, user_env_data)
+                    elif module_type == "kb":
+                        _ = run_kb.delay(module_run_data, user_env_data)
+                    elif module_type == "memory":
+                        _ = run_memory.delay(module_run_data, user_env_data)                   
+                elif module_run_input.deployment.module.execution_type == ModuleExecutionType.docker and module_type == "agent":
+                    # validate docker params
+                    try:
+                        _ = DockerParams(**module_run_data["inputs"])
+                    except Exception as e:
+                        raise HTTPException(status_code=400, detail=f"Invalid docker params: {str(e)}")
+                    _ = execute_docker_agent.delay(module_run_data)
+                else:
+                    raise HTTPException(status_code=400, detail=f"Invalid {module_type} run type")
 
-            return module_run_data
+                return module_run_data
         
         except HTTPException as e:
             logger.error(f"Error: {str(e.detail)}")